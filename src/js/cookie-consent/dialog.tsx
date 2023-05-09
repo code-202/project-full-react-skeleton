815e58d822a7eac5e8f78e7982c556500988ee07
jngermon <jn.germon@gmail.com>
2023-05-09 21:33:20 +0200

    Update cookie-consent customization

--- src/js/cookie-consent/dialog.tsx
+++ src/js/cookie-consent/dialog.tsx
@@ -1,27 +1,111 @@
 import * as React from 'react'
-import { Dialog as Base, Store } from '@code-202/cookie-consent'
-import Icon from '@mdi/react';
-import {  mdiCheck, mdiClose } from '@mdi/js';
+import { Dialog as Base, DialogProps } from '@code-202/cookie-consent'
+import Icon from '@mdi/react'
+import {  mdiCheck, mdiClose, mdiCog } from '@mdi/js'
+import { LocaleStore } from '@code-202/intl'
+import { getKernel } from '@code-202/kernel'
+import { Alert, Badge, Collapse } from 'reactstrap'
 
-export default class Dialog extends Base {
+interface Props {
 
-    renderModalHeader (): React.ReactNode {
-        return 'Votre choix'
+}
+
+export default class Dialog extends React.PureComponent<Props> {
+    protected localeStore: LocaleStore
+
+    constructor(props: Props) {
+        super(props)
+
+        this.localeStore = getKernel().container.get('intl.locale') as LocaleStore
     }
 
-    renderModalBody (): React.ReactNode {
-        return 'Ce site utilise des cookies afin de vous faire profiter d\'une navigation optimale'
-    }
+    render(): React.ReactNode {
 
-    renderButtonAcceptAll (): React.ReactNode {
-        return <>
-            <Icon path={mdiCheck} size="1em" /> Tout accepter
-        </>
-    }
+        const intl = this.localeStore.intl
 
-    renderButtonDeclineAll (): React.ReactNode {
-        return <>
-            <Icon path={mdiClose} size="1em" /> Tout refuser
-        </>
+        const props: DialogProps = {
+            className: 'modal-lg',
+            header: {
+                content: (store) => intl.formatMessage({id: 'app.cookie-consent.title'})
+            },
+            body: {
+                content: (store, customizing) => <>
+                    { intl.formatMessage({id: 'app.cookie-consent.description'}) }
+
+                    { store.newServiceSinceLastConsent && (
+                        <Alert color="info">
+                            { intl.formatMessage({id: 'app.cookie-consent.description-new'}) }
+                        </Alert>
+                    )}
+
+                    <Collapse isOpen={store.customizing}>
+                        { customizing }
+                    </Collapse>
+                </>
+            },
+            footer: {
+                acceptAll: {
+                    content: (store) => <>
+                            <Icon path={mdiCheck} size="1em" /> {this.localeStore.intl.formatMessage({id: 'app.cookie-consent.accept_all'})}
+                        </>
+                },
+                declineAll: {
+                    content: (store) => <>
+                            <Icon path={mdiClose} size="1em" /> {this.localeStore.intl.formatMessage({id: 'app.cookie-consent.decline_all'})}
+                        </>
+                },
+                customize: {
+                    content: (store) => <>
+                            <Icon path={mdiCog} size="1em" /> {this.localeStore.intl.formatMessage({id: 'app.cookie-consent.customize'})}
+                        </>
+                },
+                close: {
+                    content: (store) => <>
+                            <Icon path={mdiClose} size="1em" /> {this.localeStore.intl.formatMessage({id: 'app.cookie-consent.close'})}
+                        </>
+                }
+            },
+            customize: {
+                type: {
+                    name: {
+                        btn: {
+                            outline: (type) => true,
+                            className: (type) => 'me-2 border-0',
+                        },
+                        content: (type) => this.localeStore.intl.formatMessage({id: `app.cookie-consent.type.${type.id}`})
+                    },
+                    acceptAll: {
+                        content: (type) => this.localeStore.intl.formatMessage({id: 'app.cookie-consent.accept_all'})
+                    },
+                    declineAll: {
+                        content: (type) => this.localeStore.intl.formatMessage({id: 'app.cookie-consent.decline_all'})
+                    },
+                    noNeedConsent: (type) => this.localeStore.intl.formatMessage({id: 'app.cookie-consent.required'}),
+                    service: {
+                        className: (service) => 'd-flex justify-content-between mb-2',
+                        name: (service) => <div>
+                                { this.localeStore.intl.formatMessage({id: `app.cookie-consent.type.${service.type}.${service.id}`})}
+                                &nbsp;:
+                                { service.cookies?.map((cookie) => (
+                                    <Badge className="ms-2" key={cookie}>{ cookie }</Badge>
+                                ))}
+                            </div>,
+                        accept: {
+                            content: (service) => this.localeStore.intl.formatMessage({id: 'app.cookie-consent.accept'})
+                        },
+                        decline: {
+                            content: (service) => this.localeStore.intl.formatMessage({id: 'app.cookie-consent.decline'})
+                        },
+                        noNeedConsent: (service) => this.localeStore.intl.formatMessage({id: 'app.cookie-consent.required'})
+                    }
+                }
+            }
+        }
+
+        return (
+            <Base
+                {...props}
+            />
+        )
     }
 }
--- src/js/cookie-consent/launcher.tsx
+++ src/js/cookie-consent/launcher.tsx
@@ -3,13 +3,13 @@ import { Launcher as Base } from '@code-202/cookie-consent'
 import Icon from '@mdi/react';
 import { mdiCookie } from '@mdi/js';
 
-export default class Launcher extends Base {
-    renderContent (): React.ReactNode {
+export default class Launcher extends React.PureComponent {
+    render (): React.ReactNode {
         return (
-            <small>
-                <Icon path={mdiCookie} size={1} className="me-2"/>
-                Gestion des cookies
-            </small>
+            <Base
+                className='cookie-content-launcher-btn border-0 text-white'
+                content={(store) => <Icon path={mdiCookie} size={1}/> }
+            />
         )
     }
 }
